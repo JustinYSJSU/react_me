c54e68f6859954c352498d08aaca0ac35f6c0415
JustinYSJSU <justin.yamamoto@sjsu.edu>
2024-08-05 12:16:50 -0700

    appbar display mobile

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -35,102 +35,141 @@ function ResponsiveAppBar() {
 
   return (
     <>
-    <AppBar
-      position="static"
-      sx={{
-        backgroundColor: '#008080',
-        width: '100%',
-        borderBottom: '2px solid #000',
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-      }}
-    >
-      <Container maxWidth={false} sx={{ paddingLeft: 0, paddingRight: 0 }}>
-        <Toolbar disableGutters sx={{ textAlign: 'left', paddingLeft: 0 }}>
+  <AppBar
+    position="static"
+    sx={{
+      backgroundColor: '#008080',
+      width: '100%',
+      borderBottom: '2px solid #000',
+      zIndex: (theme) => theme.zIndex.drawer + 1,
+    }}
+  >
+    <Container maxWidth={false} sx={{ paddingLeft: 0, paddingRight: 0 }}>
+      <Toolbar disableGutters sx={{ textAlign: 'left', paddingLeft: 0 }}>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          onClick={handleOpenNavMenu}
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'Arial',
+            fontWeight: 700,
+            letterSpacing: '.1rem',
+            color: 'inherit',
+            textDecoration: 'none',
+            cursor: 'pointer',  // Add cursor pointer for better UX
+          }}
+        >
+          Justin Yamamoto
+        </Typography>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+          </IconButton>
           <Typography
             variant="h6"
             noWrap
             component="a"
+            onClick={handleOpenNavMenu}
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              flexGrow: 1,
+              display: { xs: 'flex', md: 'none' },
               fontFamily: 'Arial',
               fontWeight: 700,
               letterSpacing: '.1rem',
               color: 'inherit',
               textDecoration: 'none',
+              cursor: 'pointer',  // Add cursor pointer for better UX
             }}
           >
             Justin Yamamoto
           </Typography>
-  
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={() => { handleCloseNavMenu(); navigate(`/${page.toLowerCase()}`); }}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-start' }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={() => navigate(`/${page.toLowerCase()}`)}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+              onClick={handleOpenUserMenu}
               color="inherit"
             >
             </IconButton>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                flexGrow: 1,
-                display: { xs: 'flex', md: 'none' },
-                fontFamily: 'Arial',
-                fontWeight: 700,
-                letterSpacing: '.1rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              Justin Yamamoto
-            </Typography>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={() => navigate(`/${page.toLowerCase()}`)}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-  
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-start' }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => navigate(`/${page.toLowerCase()}`)}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {/* Add menu items here */}
+            <MenuItem onClick={handleCloseUserMenu}>Profile</MenuItem>
+            <MenuItem onClick={handleCloseUserMenu}>My account</MenuItem>
+            <MenuItem onClick={handleCloseUserMenu}>Logout</MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </Container>
+  </AppBar>
+</>
 
-        </Toolbar>
-      </Container>
-    </AppBar>
-  </>
   
 
 
