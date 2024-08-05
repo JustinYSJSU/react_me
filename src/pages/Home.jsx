6c935ca7f8a76eb3e9ee7f9830b061a65848ade9
JustinYSJSU <justin.yamamoto@sjsu.edu>
2024-08-05 11:44:29 -0700

    mobile change for project display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -114,147 +114,141 @@ export const Home = () => {
         </Typography>
       </Box>
       <Box sx={{ flexGrow: 1, padding: '20px' }}>
-        <Grid container spacing={3} justifyContent="center" wrap="nowrap">
-          <Grid item xs={12} sm={6} md={4} lg={2.4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-              <CardMedia
+      <Grid container spacing={3} justifyContent="center">
+    <Grid item xs={12} sm={6} md={4} lg={2.4}>
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
                 sx={{ height: 140 }}
                 image="https://firebasestorage.googleapis.com/v0/b/react-valorant.appspot.com/o/other%2Fc03d9a0a309efab3779d74bfdb082b363cef99e8-854x484.png?alt=media&token=407d64eb-be0a-43bf-b9fd-519f697f2b71"
                 title="video review"
-              />
-              <CardContent>
+            />
+            <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
-                  Video Review Application
+                    Video Review Application
                 </Typography>
                 <Typography variant="h4" sx={{ fontSize: '18px' }}>
-                  React application designed to help VALORANT players improve their skills.
+                    React application designed to help VALORANT players improve their skills.
                 </Typography>
-              </CardContent>
-              <CardActions sx={{ mt: 'auto' }}>
+            </CardContent>
+            <CardActions sx={{ mt: 'auto' }}>
                 <a href="/videoreview">
-                  <Button> Learn More </Button>
+                    <Button> Learn More </Button>
                 </a>
                 <a href="https://valorantvodreview.vercel.app/">
-                  <Button> Demo </Button>
+                    <Button> Demo </Button>
                 </a>
+            </CardActions>
+        </Card>
+    </Grid>
 
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4} lg={2.4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-              <CardMedia
+    <Grid item xs={12} sm={6} md={4} lg={2.4}>
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
                 sx={{ height: 140 }}
                 image="https://firebasestorage.googleapis.com/v0/b/react-valorant.appspot.com/o/other%2F72301087-2.jpg?alt=media&token=2103a903-fcf7-4ac3-9c5c-7a09f6efa5ef"
                 title="social"
-              />
-              <CardContent>
+            />
+            <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
-                  Social Community Site
+                    Social Community Site
                 </Typography>
                 <Typography variant="h4" sx={{ fontSize: '18px' }}>
-                  Full Stack Web application focused on community and connection. Developed with MySQL, Express.js, React.js, and Node.js
+                    Full Stack Web application focused on community and connection. Developed with MySQL, Express.js, React.js, and Node.js
                 </Typography>
-              </CardContent>
-              <CardActions sx={{ mt: 'auto' }}>
+            </CardContent>
+            <CardActions sx={{ mt: 'auto' }}>
                 <a href="/socialcomm">
-                  <Button> Learn More</Button>
+                    <Button> Learn More</Button>
                 </a>
 
                 <a href="https://github.com/shyamvyas890/ASMREncyclopedia">
-                  <Button> GitHub </Button>
+                    <Button> GitHub </Button>
                 </a>
+            </CardActions>
+        </Card>
+    </Grid>
 
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4} lg={2.4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-              <CardMedia
+    <Grid item xs={12} sm={6} md={4} lg={2.4}>
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
                 sx={{ height: 140 }}
                 image="https://firebasestorage.googleapis.com/v0/b/react-valorant.appspot.com/o/other%2Fspotify-playlist.jpeg?alt=media&token=4444ec66-97d6-4a80-9df3-65fc0a3baadd"
                 title="spotify"
-              />
-              <CardContent>
+            />
+            <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
-                  Spotify Summary
+                    Spotify Summary
                 </Typography>
                 <Typography variant="h4" sx={{ fontSize: '18px' }}>
-                  React application to give users information on their top Spotify songs and artists.
+                    React application to give users information on their top Spotify songs and artists.
                 </Typography>
-              </CardContent>
-              <CardActions sx={{ mt: 'auto' }}>
+            </CardContent>
+            <CardActions sx={{ mt: 'auto' }}>
                 <a href="/spotify">
-                  <Button> Learn More </Button>
+                    <Button> Learn More </Button>
                 </a>
 
                 <a href="https://github.com/JustinYSJSU/react_spotify">
-                  <Button> GitHub </Button>
+                    <Button> GitHub </Button>
                 </a>
+            </CardActions>
+        </Card>
+    </Grid>
 
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4} lg={2.4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-              <CardMedia
+    <Grid item xs={12} sm={6} md={4} lg={2.4}>
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
                 sx={{ height: 140 }}
                 image="https://firebasestorage.googleapis.com/v0/b/react-valorant.appspot.com/o/other%2Fmaze.jpg?alt=media&token=e3bad8cf-cc33-471e-91db-641bd62c3776"
                 title="maze"
-              />
-              <CardContent>
+            />
+            <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
-                  Maze Generator and Solver
+                    Maze Generator and Solver
                 </Typography>
                 <Typography variant="h4" sx={{ fontSize: '18px' }}>
-                  Java program utilizing data structures and algorithms to generate and solve mazes.
+                    Java program utilizing data structures and algorithms to generate and solve mazes.
                 </Typography>
-              </CardContent>
-              <CardActions sx={{ mt: 'auto' }}>
+            </CardContent>
+            <CardActions sx={{ mt: 'auto' }}>
                 <a href="/mazegensolve">
-                  <Button> Learn More </Button>
+                    <Button> Learn More </Button>
                 </a>
 
                 <a href="https://github.com/JustinYSJSU/MazeGenerator">
-                  <Button> GitHub </Button>
+                    <Button> GitHub </Button>
                 </a>
+            </CardActions>
+        </Card>
+    </Grid>
 
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4} lg={2.4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-              <CardMedia
+    <Grid item xs={12} sm={6} md={4} lg={2.4}>
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
                 sx={{ height: 140 }}
                 image="https://firebasestorage.googleapis.com/v0/b/react-valorant.appspot.com/o/other%2Fimages.jpg?alt=media&token=65cbef39-1853-4320-ad66-0d4f0e866139"
                 title="chat"
-              />
-              <CardContent>
+            />
+            <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: "bold" }}>
-                  Chat Application
+                    Chat Application
                 </Typography>
                 <Typography variant="h4" sx={{ fontSize: '18px' }}>
-                  Python chat application allowing users to communicate over a local network
+                    Python chat application allowing users to communicate over a local network
                 </Typography>
-              </CardContent>
-              <CardActions sx={{ mt: 'auto' }}>
+            </CardContent>
+            <CardActions sx={{ mt: 'auto' }}>
                 <a href="/chatapplication">
-                  <Button> Learn more </Button>
+                    <Button> Learn More </Button>
                 </a>
 
                 <a href="https://github.com/YueYingLee/CMPE-148">
-                  <Button> GitHub </Button>
+                    <Button> GitHub </Button>
                 </a>
-
-              </CardActions>
-            </Card>
-          </Grid>
-
-        </Grid>
+            </CardActions>
+        </Card>
+    </Grid>
+</Grid>
       </Box>
       <Typography variant="h2" sx={{ textAlign: 'left', marginLeft: '20px', fontWeight: 'bold', fontSize: "35px" }}>
         Contact Me
